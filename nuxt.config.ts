139b3659269016b8c1dd0fa21b0a6d72d9379475
robertt@clearlakesdental.com <robertt@clearlakesdental.com>
2025-11-13 15:54:18 -0600

    split from single page into multiple, added more guiding comments and nuxt examples

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,10 @@ export default defineNuxtConfig({
     redirect: false,
     // create your own .env file and add your supabase url and key
     url: process.env.SUPABASE_URL,
+    // can be found in supabase dashboard under project settings -> Data API
+
     key: process.env.SUPABASE_KEY,
     serviceKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    // can be found in supabase dashboard under project settings -> API Keys
   },
 });
--- server/api/fetchData.ts
+++ server/api/fetchData.ts
@@ -1,13 +1,12 @@
 import { serverSupabaseClient } from "#supabase/server";
 import type { SupabaseClient } from "@supabase/supabase-js";
 
-// example of how to get data from supabase though Nuxt's server API
-//     notice how we need to use a different client
+// Example of how to get data from supabase though Nuxt's server API
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const client = await serverSupabaseClient(event);
-  const { data, error } = await getTableData(client, body);
+  const { data, error } = await getTableData(client, body.table);
   //   const { data, error } = await getStorageUrl(client, body);
   if (error && error.message) {
     throw createError({
@@ -18,14 +17,17 @@ export default defineEventHandler(async (event) => {
   return data;
 });
 
-async function getTableData(client: SupabaseClient, body: any) {
-  const { data, error } = await client.from(body.table).select("*");
+// Quick example of how to query a table
+async function getTableData(client: SupabaseClient, tableName: string) {
+  const { data, error } = await client.from(tableName).select("*");
   return { data, error };
 }
 
+// Quick example of how to get an images public url. Will not work if a bucket is private.
 async function getStorageUrl(client: SupabaseClient, body: any) {
-  const { data, error } = await client.storage
+  const { data } = await client.storage
     .from(body.bucket)
-    .createSignedUrl(body.fileName, 300);
-  return { data, error };
+    .getPublicUrl(body.fileName);
+
+  return data;
 }
